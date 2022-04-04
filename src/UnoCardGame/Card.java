bba6ecb28d7bde0920eb590f4a2e06e650ada7ff
Kimio Nishino <kimionishino1@gmail.com>
2022-04-04 02:04:18 -0400

    Created new CardProperties class. Card class is now extended to CardProperties.

--- src/UnoCardGame/Card.java
+++ src/UnoCardGame/Card.java
@@ -1,8 +1,18 @@
+
+import UnoCardGame.CardProperties;
+
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
  */
+
+
+
+
+
+
+
 
 /**
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
@@ -10,17 +20,56 @@
  *
  * @author dancye
  * @author emszw Emily Szwalek Feb 2022
- * @author Kimio Nishino Feb 2022
+ * @modifier Kimio Nishino Feb 2022
  */
-public abstract class Card {
+public class Card extends CardProperties{
     //default modifier for child classes
+    private CardColor color;
+    private CardNumber number;
+    private CardRank rank;
+
+    public Card(CardColor color, CardNumber number, CardRank rank) {
+        this.color = color;
+        this.number = number;
+        this.rank = rank;
+    }
+    
+    
+    
+    public CardColor getColor() {
+        return color;
+    }
+
+    public void setColor(CardColor color) {
+        this.color = color;
+    }
+
+    public CardNumber getNumber() {
+        return number;
+    }
+
+    public void setNumber(CardNumber number) {
+        this.number = number;
+    }
+
+    public CardRank getRank() {
+        return rank;
+    }
+
+    public void setRank(CardRank rank) {
+        this.rank = rank;
+    }
+    
+    
+    
+    
 
     /**
      * Students should implement this method for their specific children classes
      *
      * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
      */
-    @Override
-    public abstract String toString();
+//    @Override
+//    public abstract String toString();
 
 }
